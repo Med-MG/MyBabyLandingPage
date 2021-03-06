58ee26bb8ed0405ea1d723caf4d73c482757a765
MOUIGUINA MOHAMED <mohamedmouiguina7@gmail.com>
2021-03-06 16:37:36 +0100

    form validation ajax

--- src/js/index.js
+++ src/js/index.js
@@ -8,3 +8,22 @@ $(window).on('scroll',function(){
   }
 });
 
+
+$(document).ready(function() {
+  $(".buy__form").submit(function(event){
+    event.preventDefault();
+    console.log('clickedddd');
+    var fullname = $('#fullname').val();
+    var phone = $('#phone').val();
+    var phoneConfirmation = $('#phone2').val();
+    var adresse = $('#adresse').val();
+    if( phone == "" || phoneConfirmation == ""){
+      $(".errorconf").html("phone fields are required");
+    }
+    if(phone != phoneConfirmation){
+      $(".errorconf").html("phone numbers are not the same");
+      $('#phone').css({"border": "1px solid #F56E96"});
+      $('#phone2').css({"border": "1px solid #F56E96"});
+    }
+  })
+})
